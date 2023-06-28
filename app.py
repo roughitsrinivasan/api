42940f323cd50a988c6e54e6e4dd9a01ecd448fa
Sudhir <sudirsha02@gmail.com>
2023-06-28 08:23:10 +0530

    added email functionality

--- app.py
+++ app.py
@@ -5,16 +5,29 @@ from flask_cors import CORS, cross_origin
 import os
 import sys
 
+from send_email import send_email
 app = Flask(__name__)
 cors = CORS(app)
 
 
+@app.route('/search', methods=['POST'])
+def index():
+    if request.method == 'POST':
+        args = request.args
+        print(args)
+        return jsonify(args)
+    else:
+        return "Hello World"
 @app.route("/image", methods=['GET', 'POST'])
 def image():
     if(request.method == "POST"):
+        args = request.args
+        name=args['name']
+        email=args['email']
         bytesOfImage = request.get_data()
         with open('image.jpeg', 'wb') as out:
             out.write(bytesOfImage)
+        send_email('image.jpeg',name,email)
         return "Image read"
 
 
@@ -25,6 +38,7 @@ def video():
         with open('video.mp4', 'wb') as out:
             out.write(bytesOfVideo)
         return "Video read"
+    
     
 if __name__ == '__main__':
     app.run(host="localhost", port=5000,debug=True)
