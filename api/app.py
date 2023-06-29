8e2a0f0d12fa81fab3722c7400fb2aa76b225379
Sudhir <sudirsha02@gmail.com>
2023-06-29 20:12:52 +0530

    removed writing approach for image transmission added stream based mail attachment

--- api/app.py
+++ api/app.py
@@ -1,19 +1,37 @@
+
+
+# Imports for SMTP
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+ 
+# Imports for Flask
 from flask import Flask, Response, request, jsonify
-from io import BytesIO
-import base64
-from flask_cors import CORS, cross_origin
-import os
-import sys
+from flask_cors import CORS
+import os,io
+
 
 app = Flask(__name__)
 cors = CORS(app)
+
+FILE_NAME='image.jpeg'
 
 @app.route('/', methods=['GET'])
 def  home():
     return "Hello WOrld"    
 
 
-@app.route('/search', methods=['POST'])
+@app.route('/test', methods=['POST','GET'])
 def index():
     if request.method == 'POST':
         args = request.args
@@ -21,69 +39,52 @@ def index():
         return jsonify(args)
     else:
         return "Hello World"
+    
+
 @app.route("/image", methods=['GET', 'POST'])
 def image():
     if(request.method == "POST"):
         args = request.args
-        name=args['name']
-        email=args['email']
+        name=args.get('name')
+        email=args.get('email')
+        if name is None:
+            return "Name is not provided"
+        if email is None:
+            return "Email is not provided"
+        
         bytesOfImage = request.get_data()
-        print(type(bytesOfImage))
-        __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        with open(os.path.join(__location__, 'image.jpeg'),'wb') as out:
-            out.write(bytesOfImage)
-        send_email('image.jpeg',name,email)
-        return "Image read"
+        imageStream = io.BytesIO(bytesOfImage)
+
+        # print('bytesOfImage',type(bytesOfImage),'imageStream',type(imageStream))
+
+        response    = send_email(name,email,imageStream)
+        if response:
+            return "Email Sent"
+        else:
+            return "Email Failed"
 
 @app.route('/send',methods=['GET'])
 def send():
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    with open(os.path.join(__location__, 'image.jpeg'),'rb') as binary_pdf:
+    with open(os.path.join(__location__, FILE_NAME),'rb') as binary_pdf:
         bytesOfImage=binary_pdf.read()
         return Response(bytesOfImage, mimetype='image/jpeg')
-    
-    
 
-
-@app.route("/video", methods=['GET', 'POST'])
-def video():
-    if(request.method == "POST"):
-        bytesOfVideo = request.get_data()
-        with open('video.mp4', 'wb') as out:
-            out.write(bytesOfVideo)
-        return "Video read"
-    
     
 
 
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 
 
-def send_email(pdfname,receiver_name,receiver_email):
+def send_email(receiver_name,receiver_email,binary_img):
     body = f'''
     Hello, {receiver_name}
-    This is the body of the email
-    sicerely yours
-    G.G.
+    Welcome to SJCE 
+    Yours Friendly
+    Prakash Annanin Vizhudhugal
     '''
     # put your email here
     sender = 'yuvibro1211@gmail.com'
-    # get the password in the gmail (manage your google account, click on the avatar on the right)
-    # then go to security (right) and app password (center)
-    # insert the password and then choose mail and this computer and then generate
-    # copy the password generated here
     password = 'tiecqtzjasbhectc'
     # put the email of the receiver here
     try:
@@ -92,41 +93,35 @@ def send_email(pdfname,receiver_name,receiver_email):
         message['From'] = sender
         message['To'] = receiver_email
         message['Subject'] = 'This email has an attacment, a pdf file'
-
+        print('attaching body to email')
         message.attach(MIMEText(body, 'plain'))
 
+        payload = MIMEBase('application', 'octate-stream', Name=FILE_NAME)
+        # payload = MIMEBase('application', 'pdf', Name=pdfname)
+        payload.set_payload((binary_img).read())
 
+        # enconding the binary into base64
+        encoders.encode_base64(payload)
 
-        # open the file in bynary
-        __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        with open(os.path.join(__location__, 'image.jpeg'),'rb') as binary_pdf:
+        # add header with file name
+        print('attaching image to email')
+        payload.add_header('Content-Decomposition', 'attachment', filename=FILE_NAME)
+        message.attach(payload)
 
-            payload = MIMEBase('application', 'octate-stream', Name=pdfname)
-            # payload = MIMEBase('application', 'pdf', Name=pdfname)
-            payload.set_payload((binary_pdf).read())
+        #use gmail with port
+        session = smtplib.SMTP('smtp.gmail.com', 587)
 
-            # enconding the binary into base64
-            encoders.encode_base64(payload)
+        #enable security
+        session.starttls()
 
-            # add header with pdf name
-            payload.add_header('Content-Decomposition', 'attachment', filename=pdfname)
-            message.attach(payload)
+        #login with mail_id and password
+        session.login(sender, password)
 
-            #use gmail with port
-            session = smtplib.SMTP('smtp.gmail.com', 587)
-
-            #enable security
-            session.starttls()
-
-            #login with mail_id and password
-            session.login(sender, password)
-
-            text = message.as_string()
-            session.sendmail(sender, receiver_email, text)
-            session.quit()
-            print('Mail Sent')
-            return True
+        text = message.as_string()
+        session.sendmail(sender, receiver_email, text)
+        session.quit()
+        print('Mail Sent')
+        return True
     except:
         print("Mail failed")
         return False
